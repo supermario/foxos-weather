083d35a269d78a899431cba00a76d878499f16a2
Mario Rogic <hello@mario.net.au>
2015-01-06 10:54:01 +1100

    Simple scraper to build static app

---
+++ build_static.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Starts thin client and recursively wgets whole site into static folder
+
+bundle exec thin -d start
+sleep 2
+wget \
+  --recursive \
+  --page-requisites \
+  --html-extension \
+  --convert-links \
+  --no-parent \
+  --no-host-directories \
+  -P static \
+    http://localhost:3000
+bundle exec thin stop
+
+cp -rp fonts static
+cp -rp icons static
