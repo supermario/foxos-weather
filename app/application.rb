0286951d16c74e70c16fbd3a871a68f79c502e49
Mario Rogic <hello@mario.net.au>
2015-01-19 21:23:36 +1100

    Heading towards a complete ruby abstraction on react...

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,6 @@ require 'browser/http'
 require 'react'
 
 class React
-  def this
-    `this` # get this to be some meta object that compiles?
-  end
   def self.react
     Native(`React`)
   end
@@ -15,64 +12,38 @@ class React
     react.createElement(elem_name, options, *contents).to_n
   end
   def self.createClass(params)
-    react = Native(`React`)
     react.createClass(params)
   end
   def self.render(element, target)
-    react = Native(`React`)
     react.render(element, target)
   end
 end
 
+class ReactClassParams
+  def params
+    {
+      render: -> { render },
+      getInitialState: -> { getInitialState }
+    }
+  end
+end
 
-
-params = {
-  render: -> {
-    React.createElement("div", {className: "flex"},
-      React.createElement(
-        "h1", {className: "temp"},
-        `this.state.current`, React.createElement("br", {}), `this.state.high`
-      ),
-      React.createElement(
-        "h2", {className: `this.state.weather_class`},
-        React.createElement("br", {}),
-        React.createElement("span", {}, `this.state.text`)
-      )
-    )
-  },
-  getInitialState: -> {
+class TestDiv < ReactClassParams
+  def render
+    React.createElement("div", {}, "test")
+  end
+  def getInitialState
     {
       high: 'h',
       current: 'c',
       weather_class: '..'
     }.to_n
-  },
-  componentDidMount: -> {
-    response = Browser::HTTP.get! `this.props.source` do
-      @headers = []
-    end
-    results = response.json['query']['results']['channel']['item']
-    forecast = results['forecast'][0]
-    current_temp = results['condition']['temp'];
+  end
+end
 
-    state = {
-      weather_class: "klimato-" + forecast['code'],
-      date: forecast['date'],
-      day: forecast['day'],
-      current: current_temp,
-      high: forecast['high'],
-      text: forecast['text'],
-    }
-    `debugger`
-    `this.setState(#{state.to_n})`
-  }
-}
-
-react_class = React.createClass(params)
-
-source = "http://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast WHERE woeid=22720989 and u='c'&format=json"
+react_class = React.createClass(TestDiv.new.params)
 
 React.render(
-  React.createElement(react_class, {source: source}),
+  React.createElement(react_class),
   $document['weather']
 )
